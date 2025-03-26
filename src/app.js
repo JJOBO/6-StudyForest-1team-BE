81270c820d4ee7c1e5b84de7d4d2350a9f2cb834
jyeon <bjlyeon2003@naver.com>
2025-03-26 16:25:50 +0900

    Modify app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+
+// 라우트 불러오기
+import focusRouter from "./modules/focus.module.js";
 import habitsRouter from "./modules/habits.module.js";
 
+// 환경변수 불러오기
 dotenv.config();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/study", habitsRouter);
 
+// API 라우팅
+
+app.use("/study", focusRouter);
+
+// 서버 실행
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-  console.log(`Server started to listen at port number ${PORT}...`);
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+
+  app.use("/study", habitsRouter);
 });

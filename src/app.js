7de05c11e1d13d44075e14c58f7031235d207b53
root <root@DESKTOP-KS7E5CV.>
2025-03-26 14:56:06 +0900

    feat: 오늘의집중 api 추가

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import cors from "cors";
 
 // 라우트 불러오기
 import focusRouter from "./modules/focus.module.js";
-import studyRouter from "./modules/study.module.js";
 
 // 환경변수 불러오기
 dotenv.config();
@@ -17,7 +16,6 @@ app.use(express.json());
 // API 라우팅
 
 app.use("/studies", focusRouter);
-app.use("/studies", studyRouter);
 
 // 서버 실행
 const PORT = process.env.PORT || 5000;

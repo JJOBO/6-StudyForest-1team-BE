3426e4edb209fc5503dea292ae1cecba2ee9396d
LEE JI SU <afafmm1903@gmail.com>
2025-03-26 16:48:28 +0900

    refactor: 코드,파일명 다른 기능과 통합(재수정)

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
-import studyRouter from "./modules/studyController.js";
+
+// 라우트 불러오기
+import studyRouter from "./modules/study.module.js";
+
+// 환경변수 불러오기
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", studyRouter);
+// API 라우팅
 
-export default app;
+app.use("/", studyRouter);
+
+// 서버 실행
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
